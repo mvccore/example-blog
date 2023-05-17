7725f1b572fe27193e1a7e67365e605918a1e5ee
Tom Flídr <tomflidr@gmail.com>
2023-05-17 17:26:56 +0200

    Release v5.2.0

--- App/Models/Base.php
+++ App/Models/Base.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use \MvcCore\Ext\Models\Db\Attrs;
 
 /** 
- * @connection my57
+ * @connection lite
  */
-#[Attrs\Connection('my57')]
+#[Attrs\Connection('lite')]
 class Base
-extends \MvcCore\Ext\Models\Db\Models\MySql
-//extends \MvcCore\Ext\Models\Db\Models\SQLite
+extends \MvcCore\Ext\Models\Db\Models\SQLite
+//extends \MvcCore\Ext\Models\Db\Models\MySql
 //extends \MvcCore\Ext\Models\Db\Models\SqlSrv
 //extends \MvcCore\Ext\Models\Db\Models\PgSql
 {
